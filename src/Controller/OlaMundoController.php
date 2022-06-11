13ade04d7a310d6c933914b13b5e3bb91200583c
Vinicius Dias <carlosv775@gmail.com>
2022-06-11 15:58:10 -0300

    Video 2.2

--- src/Controller/OlaMundoController.php
+++ src/Controller/OlaMundoController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OlaMundoController
 {
-    #[Route('/ola_mundo', name: 'app_ola_mundo')]
-    public function index(): Response
+    #[Route('/ola_mundo')]
+    public function index(Request $request): Response
     {
-        return new Response('<h1>Olá mundo</h1>' . $erro);
+        return new Response(
+            '<h1>ID:</h1>' . $request->query->get('id'),
+            401,
+            [
+                'X-Qualquer-Coisa' => 'Valor'
+            ]
+        );
     }
 }
