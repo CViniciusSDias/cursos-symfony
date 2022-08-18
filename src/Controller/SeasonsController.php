20130c3657544894f8f8a2d69aed2644b48d1e4d
Vinicius Dias <carlosv775@gmail.com>
2022-08-18 16:33:05 -0300

    Video 2.2

--- src/Controller/SeasonsController.php
+++ src/Controller/SeasonsController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use App\Entity\Series;
+use Doctrine\ORM\PersistentCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class SeasonsController extends AbstractController
 {
+    public function __construct(private CacheInterface $cache)
+    {
+    }
+
     #[Route('/series/{series}/seasons', name: 'app_seasons')]
     public function index(Series $series): Response
     {
-        $seasons = $series->getSeasons();
+        $seasons = $this->cache->get(
+            "series_{$series->getId()}_seasons",
+            function (ItemInterface $item) use ($series) {
+                $item->expiresAfter(new \DateInterval('PT10S'));
+
+                /** @var PersistentCollection $seasons */
+                $seasons = $series->getSeasons();
+                $seasons->initialize();
+
+                return $seasons;
+            }
+        );
 
         return $this->render('seasons/index.html.twig', [
             'series' => $series,
