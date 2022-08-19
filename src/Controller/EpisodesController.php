8d56e27530b81f10cd18edaed6b9acc28a3c100b
Vinicius Dias <carlosv775@gmail.com>
2022-08-19 00:34:02 -0300

    Video 3.2

--- src/Controller/EpisodesController.php
+++ src/Controller/EpisodesController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EpisodesController extends AbstractController
 {
-    #[Route('/season/{season}/episodes', name: 'app_episodes')]
+    #[Route('/season/{season}/episodes', name: 'app_episodes', methods: ['GET'])]
     public function index(Season $season): Response
     {
         return $this->render('episodes/index.html.twig', [
@@ -17,5 +18,11 @@ class EpisodesController extends AbstractController
             'series' => $season->getSeries(),
             'episodes' => $season->getEpisodes(),
         ]);
+    }
+
+    #[Route('/season/{season}/episodes', name: 'app_watch_episodes', methods: ['POST'])]
+    public function watch(Season $season, Request $request): Response
+    {
+        dd(array_keys($request->request->all('episodes')));
     }
 }
