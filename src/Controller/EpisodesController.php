8d30f3a6bbbcc5c87b8ae745833dc526a0318543
Vinicius Dias <carlosv775@gmail.com>
2022-08-19 01:07:21 -0300

    Video 3.3

--- src/Controller/EpisodesController.php
+++ src/Controller/EpisodesController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Season;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EpisodesController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route('/season/{season}/episodes', name: 'app_episodes', methods: ['GET'])]
     public function index(Season $season): Response
     {
@@ -23,6 +29,17 @@ class EpisodesController extends AbstractController
     #[Route('/season/{season}/episodes', name: 'app_watch_episodes', methods: ['POST'])]
     public function watch(Season $season, Request $request): Response
     {
-        dd(array_keys($request->request->all('episodes')));
+        $watchedEpisodes = array_keys($request->request->all('episodes'));
+        $episodes = $season->getEpisodes();
+
+        foreach ($episodes as $episode) {
+            $episode->setWatched(in_array($episode->getId(), $watchedEpisodes));
+        }
+
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Episódios marcados como assistidos');
+
+        return new RedirectResponse("/season/{$season->getId()}/episodes");
     }
 }
