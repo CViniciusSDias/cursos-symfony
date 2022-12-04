40f4a9d3d826b56322a4e6cab51289492ac7d967
Vinicius Dias <carlosv775@gmail.com>
2022-12-03 22:12:14 -0300

    Video 4.1

--- src/MessageHandler/DeleteSeriesImageHandler.php
+++ src/MessageHandler/DeleteSeriesImageHandler.php
@@ -6,19 +6,22 @@ namespace App\MessageHandler;
 
 use App\Message\SeriesWasDeleted;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class DeleteSeriesImageHandler
 {
-    public function __construct(private ParameterBagInterface $parameterBag)
-    {
+    public function __construct(
+        private ParameterBagInterface $parameterBag,
+        private Filesystem $filesystem
+    ) {
     }
 
     public function __invoke(SeriesWasDeleted $message)
     {
         $coverImagePath = $message->series->getCoverImagePath();
-        unlink(
+        $this->filesystem->remove(
             $this->parameterBag->get('cover_image_directory') . DIRECTORY_SEPARATOR . $coverImagePath
         );
     }
